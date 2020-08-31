05a44cbcdcdf001202dcd54cf0daffefe75136f8
Adam Hicks <thomas.adam.hicks@gmail.com>
2020-08-31 10:13:22 -0600

    Updates

--- install_plex.sh
+++ install_plex.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
-sudo apt install wget -y
-wget https://downloads.plex.tv/plex-media-server-new/1.19.3.2852-219a9974e/debian/plexmediaserver_1.19.3.2852-219a9974e_amd64.deb -O /tmp/plex.deb
 
-sudo dpkg -i /tmp/plex.deb
+sudo apt update -y
+
+sudo apt install -y curl net-tools
+
+curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
+
+echo "deb https://downloads.plex.tv/repo/deb public main" | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+
+sudo apt update -y
+
+sudo apt-get -y -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew install plexmediaserver
 
 sudo systemctl enable plexmediaserver
 sudo systemctl start plexmediaserver
-
--- plex.tf
+++ plex.tf
@@ -16,10 +16,13 @@ resource "aws_default_vpc" "default" {}
 
 resource "aws_instance" "plex" {
   ami                         = "ami-075c0d7b8471e91e9"
-  key_name                    = "${aws_key_pair.plex_key.key_name}"
+  key_name                    = aws_key_pair.plex_key.key_name
   instance_type               = "t2.medium"
-  security_groups             = ["${aws_security_group.plex-sg.name}"]
+  security_groups             = [aws_security_group.plex-sg.name]
   associate_public_ip_address = true
+  tags                       = {
+    Name                  = "Plex"
+  }
 
   provisioner "file" {
     source      = "./install_plex.sh"
@@ -28,7 +31,7 @@ resource "aws_instance" "plex" {
       type    = "ssh"
       host    = aws_instance.plex.public_ip
       user    = var.username
-      private_key = "${file("~/.ssh/id_rsa")}"
+      private_key = file("~/.ssh/id_rsa")
     }
   }
 
@@ -42,7 +45,7 @@ resource "aws_instance" "plex" {
       type    = "ssh"
       host    = aws_instance.plex.public_ip 
       user    = var.username
-      private_key = "${file("~/.ssh/id_rsa")}"
+      private_key = file("~/.ssh/id_rsa")
     }
   }
 }
@@ -55,14 +58,28 @@ resource "aws_security_group" "plex-sg" {
     protocol    = "tcp"
     from_port   = 22
     to_port     = 22
+    cidr_blocks = ["67.190.146.24/32"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 51413
+    to_port     = 51413
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    protocol    = "tcp"
+    from_port   = 9091
+    to_port     = 9091
+    cidr_blocks = ["67.190.146.24/32"]
   }
   
   ingress {
     protocol    = "tcp"
     from_port   = 32400
     to_port     = 32400
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["67.190.146.24/32"]
   }
 
   egress {
@@ -71,14 +88,26 @@ resource "aws_security_group" "plex-sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 }
 
 resource "aws_key_pair" "plex_key" {
   key_name   = "ahicks_key"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
+resource "aws_ebs_volume" "Plex" {
+  availability_zone = "eu-west-3b"
+  size = 100
+  tags = {
+    Name = "Plex"
+  }
+}
+
+resource "aws_volume_attachment" "plex" {
+  device_name = "/dev/sdf"
+  volume_id = aws_ebs_volume.Plex.id
+  instance_id = aws_instance.plex.id
+}
 # ===========
 # = Outputs =
 # ===========
